c1cf468f27133ca23b2519ece9551371454a92ea
Loïc Salaï <72502553+Geneteck@users.noreply.github.com>
2022-01-10 22:48:31 +0100

    Acceuil v3.3
    Sans bug + Merge profil

--- lib/screens/authenticate/confirmation.dart
+++ lib/screens/authenticate/confirmation.dart
@@ -9,15 +9,17 @@ class Person {
 
   Person(this.name, this.surname, this.age);
 }
+
 class ConfirmationInscription extends StatefulWidget {
   ConfirmationInscription({Key? key}) : super(key: key);
 
   @override
-  _ConfirmationInscriptionState createState() => _ConfirmationInscriptionState();
+  _ConfirmationInscriptionState createState() =>
+      _ConfirmationInscriptionState();
 }
 
 class _ConfirmationInscriptionState extends State<ConfirmationInscription> {
-List? _myActivities;
+  List? _myActivities;
   late String _myActivitiesResult;
   final formKey = new GlobalKey<FormState>();
   static List<Person> people = [
@@ -44,8 +46,6 @@ List? _myActivities;
       });
     }
   }
-  
-  
 
   @override
   Widget build(BuildContext context) {
@@ -55,10 +55,10 @@ List? _myActivities;
           gradient: LinearGradient(
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
-              colors: [Color(0xff03989E ), Color(0xffF9E79F)])),
+              colors: [Color(0xff03989E), Color(0xffF9E79F)])),
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
+          body: SingleChildScrollView(
+        child: Padding(
           padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -68,183 +68,182 @@ List? _myActivities;
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
               ),
-              SizedBox(height: 30,),
-
+              SizedBox(
+                height: 30,
+              ),
               Text('Vos premiers groupes !'),
-
-              SizedBox(height: 20,),
-                  ListView.builder(
-        itemCount: people.length,
-        itemBuilder: (context, index) {
-          final Person person = people[index];
-          return ListTile(
-            title: Text(person.name),
-            subtitle: Text(person.surname),
-            trailing: Text('${person.age} yo'),
-          );
-        },
-      ),
-       FloatingActionButton(
-        tooltip: 'Search people',
-        onPressed: () => showSearch(
-          context: context,
-          delegate: SearchPage<Person>(
-            onQueryUpdate: (s) => print(s),
-            items: people,
-            searchLabel: 'Search people',
-            suggestion: Center(
-              child: Text('Filter people by name, surname or age'),
-            ),
-            failure: Center(
-              child: Text('No person found :('),
-            ),
-            filter: (person) => [
-              person.name,
-              person.surname,
-              person.age.toString(),
-            ],
-            builder: (person) => ListTile(
-              title: Text(person.name),
-              subtitle: Text(person.surname),
-              trailing: Text('${person.age} yo'),
-            ),
-          ),
-        ),
-        child: Icon(Icons.search),),
-  
-               Container(
+              SizedBox(
+                height: 20,
+              ),
+              ListView.builder(
+                itemCount: people.length,
+                itemBuilder: (context, index) {
+                  final Person person = people[index];
+                  return ListTile(
+                    title: Text(person.name),
+                    subtitle: Text(person.surname),
+                    trailing: Text('${person.age} yo'),
+                  );
+                },
+              ),
+              FloatingActionButton(
+                tooltip: 'Search people',
+                onPressed: () => showSearch(
+                  context: context,
+                  delegate: SearchPage<Person>(
+                    onQueryUpdate: (s) => print(s),
+                    items: people,
+                    searchLabel: 'Search people',
+                    suggestion: Center(
+                      child: Text('Filter people by name, surname or age'),
+                    ),
+                    failure: Center(
+                      child: Text('No person found :('),
+                    ),
+                    filter: (person) => [
+                      person.name,
+                      person.surname,
+                      person.age.toString(),
+                    ],
+                    builder: (person) => ListTile(
+                      title: Text(person.name),
+                      subtitle: Text(person.surname),
+                      trailing: Text('${person.age} yo'),
+                    ),
+                  ),
+                ),
+                child: Icon(Icons.search),
+              ),
+              Container(
                 width: double.infinity,
                 height: 100,
-                decoration :BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.red,
-                    ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.red,
+                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                  Text('Amis'),
-                ],),
+                    Text('Amis'),
+                  ],
+                ),
               ),
+              SizedBox(
+                height: 20,
               ),
-
-
-              SizedBox(height: 20,),
-
               GestureDetector(
                 onTap: () {
                   showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    scrollable: true,
-                    title: Text('Login'),
-                    content: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Form(
-                            key: formKey,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.all(16),
-                                
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(8),
-                                  child: ElevatedButton(
-                                    child: Text('Save'),
-                                    onPressed: _saveForm,
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          scrollable: true,
+                          title: Text('Login'),
+                          content: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Form(
+                              key: formKey,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.all(16),
                                   ),
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(16),
-                                  child: Text(_myActivitiesResult),
-                                )
-                              ],
+                                  Container(
+                                    padding: EdgeInsets.all(8),
+                                    child: ElevatedButton(
+                                      child: Text('Save'),
+                                      onPressed: _saveForm,
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(16),
+                                    child: Text(_myActivitiesResult),
+                                  )
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                  });
+                        );
+                      });
                 },
                 child: Container(
-                width: double.infinity,
-                height: 100,
-                decoration :BoxDecoration(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
                     color: Colors.green,
-                    ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  Text('Famille'),
-                ],),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Famille'),
+                    ],
+                  ),
+                ),
               ),
+              SizedBox(
+                height: 20,
               ),
-
-              SizedBox(height: 20,),
-
               GestureDetector(
                 onTap: () {
                   showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    scrollable: true,
-                    title: Text('Login'),
-                    content: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Form(
-                            key: formKey,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.all(16),
-                                
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(8),
-                                  child: ElevatedButton(
-                                    child: Text('Save'),
-                                    onPressed: _saveForm,
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          scrollable: true,
+                          title: Text('Login'),
+                          content: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Form(
+                              key: formKey,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.all(16),
                                   ),
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(16),
-                                  child: Text(_myActivitiesResult),
-                                )
-                              ],
+                                  Container(
+                                    padding: EdgeInsets.all(8),
+                                    child: ElevatedButton(
+                                      child: Text('Save'),
+                                      onPressed: _saveForm,
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(16),
+                                    child: Text(_myActivitiesResult),
+                                  )
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                  });
+                        );
+                      });
                 },
                 child: Container(
-                width: double.infinity,
-                height: 100,
-                decoration :BoxDecoration(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
                     color: Colors.yellow,
-                    ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  Text('Collègues'),
-                ],),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Collègues'),
+                    ],
+                  ),
+                ),
               ),
+              SizedBox(
+                height: 30,
               ),
-              SizedBox(height: 30,),
-
-              ElevatedButton(
-                onPressed: () {}, 
-                child: Text('S\'inscrire'))
+              ElevatedButton(onPressed: () {}, child: Text('S\'inscrire'))
             ],
           ),
         ),
-        )
-        ),
+      )),
     );
   }
 }
