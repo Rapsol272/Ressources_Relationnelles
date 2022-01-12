eed079da792b174ab653eab9c62c3b56ac1fa3a3
Adrizen89 <adrienberard@hotmail.fr>
2022-01-12 11:18:11 +0100

    maj search

--- lib/services/rechercheService.dart
+++ lib/services/rechercheService.dart
@@ -1,9 +1,2 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SearchService {
-  searchByName(String searchField) {
-    return FirebaseFirestore.instance
-        .collection('users')
-        .getDocuments();
-  }
-}
