36418a92682c0ee48c63704f7bce621995ad5eca
Loïc Salaï <72502553+Geneteck@users.noreply.github.com>
2022-02-15 16:57:28 +0100

    Acceuil v3.7

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 
-importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 firebase.initializeApp({
     apiKey: "AIzaSyAMYeqrgrqgwKu4kwwIp6BdtE",
